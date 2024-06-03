bf5c215b5310f6bea89f64ec2aa4e0fb45d8cdf9
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-06-03 20:02:48 +0900

    feat: records API 개발
    records API 개발 진행
    - records.module.ts 생성
    - records.controller.ts 생성
    - records.service.ts 생성
    - dtos 폴더 설정
    
    fixes: #6
    related to: #6

--- src/main/io/wisoft/capstone/gc/app.module.ts
+++ src/main/io/wisoft/capstone/gc/app.module.ts
@@ -1,9 +1,10 @@
 import { AppController } from "@gc/app.controller";
 import { AppService } from "@gc/app.service";
 import { Module } from "@nestjs/common";
+import { RecordsModule } from './records/records.module';
 
 @Module({
-  imports: [],
+  imports: [RecordsModule],
   controllers: [AppController],
   providers: [AppService],
 })
