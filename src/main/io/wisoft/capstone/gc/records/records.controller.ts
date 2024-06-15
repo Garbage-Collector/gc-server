8e7a8f1744dbd47389685d6edf128696a7276424
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-06-15 15:48:23 +0900

    feat: Swagger 설정
    Swagger 설정 진행
    - records Swagger 설정
    
    fixes: #10
    related to: #10

--- src/main/io/wisoft/capstone/gc/records/records.controller.ts
+++ src/main/io/wisoft/capstone/gc/records/records.controller.ts
@@ -18,14 +18,30 @@ import {
   UseInterceptors,
 } from "@nestjs/common";
 import { FilesInterceptor } from "@nestjs/platform-express";
+import {
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from "@nestjs/swagger";
 
 @Controller("/api/records")
+@ApiTags("Records API")
 export class RecordsController {
   constructor(private readonly recordService: RecordsService) {}
 
   // 기록 생성
   @Post(":userId")
   @UseInterceptors(FilesInterceptor("images", 5, multerOptions("images")))
+  @ApiOperation({
+    summary: "기록 생성",
+    description:
+      "기록을 생성할 때는 title, location, startTime, endTime, createdAt, content, image 가 필요",
+  })
+  @ApiCreatedResponse({
+    description: "기록에 대한 고유한 ID 생성함",
+    type: RecordsCreateRequestDto,
+  })
   async createRecord(
     @Body() recordsCreateRequestDto: RecordsCreateRequestDto,
     @UploadedFiles() files: Array<Express.Multer.File>,
@@ -40,6 +56,19 @@ export class RecordsController {
 
   // 기록 다건 조회
   @Get(":userId")
+  @ApiOperation({
+    summary: "한 명의 사용자에 대한 기록 조회(多)",
+    description:
+      "한 명의 사용자에 대한 모든 기록을 보여줌(지금은 사용자가 한 명이기 때문에 userId가 필요)",
+  })
+  @ApiParam({
+    name: "userId",
+    type: "number",
+    description: "userId는 number로 접근함",
+  })
+  @ApiCreatedResponse({
+    description: "한 명의 사용자에 대한 모든 기록을 보여줌",
+  })
   async getAllRecords(
     @Param("userId") param: string,
   ): Promise<RecordsGetAllRecordsResponseDto> {
@@ -48,6 +77,23 @@ export class RecordsController {
 
   // 기록 단건 조회
   @Get(":userId/:recordId")
+  @ApiOperation({
+    summary: "한 명의 사용자에 대한 기록 조회(少)",
+    description: "한 명의 사용자에 대한 하나의 기록을 보여줌",
+  })
+  @ApiParam({
+    name: "userId",
+    type: "number",
+    description: "userId는 number로 접근함",
+  })
+  @ApiParam({
+    name: "recordId",
+    type: "string",
+    description: "(기록 uuid)1666b109-ea53-4db8-8cc7-903c87453425",
+  })
+  @ApiCreatedResponse({
+    description: "한 명의 사용자에 대한 하나 기록을 보여줌",
+  })
   async getRecord(
     @Param("userId") userId: string,
     @Param("recordId") recordId: string,
@@ -61,6 +107,10 @@ export class RecordsController {
   // 기록 수정
   @Put(":recordId")
   @UseInterceptors(FilesInterceptor("images", 5, multerOptions("images")))
+  @ApiOperation({
+    summary: "기록에 대한 수정",
+    description: "기록을 수정할 수 있음",
+  })
   async updateRecord(
     @Param("recordId") recordId: string,
     @Body() updateRecordDto: RecordsUpdateRequestDto,
@@ -71,6 +121,18 @@ export class RecordsController {
 
   // 기록 삭제
   @Delete(":recordId")
+  @ApiOperation({
+    summary: "기록 삭제",
+    description: "한 명의 사용자에 대한 기록을 지움",
+  })
+  @ApiParam({
+    name: "recordId",
+    type: "string",
+    description: "(기록 uuid)1666b109-ea53-4db8-8cc7-903c87453425",
+  })
+  @ApiCreatedResponse({
+    description: "{status: 204}",
+  })
   async removeRecord(
     @Param("recordId") param: string,
   ): Promise<RecordsHttpStatusDto> {
