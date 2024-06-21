6c128c85d3fa82ba18e99e34bf42a1fe45dfd576
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-06-21 14:05:20 +0900

    feat: add verifying email html
    resolves: #23

--- src/main/io/wisoft/capstone/gc/auth/mail/mail.service.ts
+++ src/main/io/wisoft/capstone/gc/auth/mail/mail.service.ts
@@ -16,7 +16,66 @@ export class MailService {
     await this.mailerService.sendMail({
       to: email,
       subject: "Garbage-Collector Email Verification",
-      text: `Your verification code is: ${verifyCode}`,
+      html: `
+        <!DOCTYPE html>
+        <html lang="ko">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Enter Verification Code</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f2f2f2;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                }
+                .container {
+                    background-color: #fff;
+                    padding: 20px;
+                    border-radius: 8px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    width: 300px;
+                }
+                .container h2 {
+                    margin-top: 0;
+                    font-size: 24px;
+                    text-align: center;
+                }
+                .form-group {
+                    margin-bottom: 15px;
+                }
+                .form-group label {
+                    display: block;
+                    margin-bottom: 5px;
+                    font-weight: bold;
+                }
+                .code-box {
+                    width: 100%;
+                    padding: 8px;
+                    box-sizing: border-box;
+                    border: 1px solid #ccc;
+                    border-radius: 4px;    
+                    text-align: center;
+                    font-size: 16px;
+                    color: #333;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h2>GC 이메일 인증번호</h2>
+                <div class="form-group">
+                    <label for="verificationCode">인증번호</label>
+                    <div class="code-box">${verifyCode}</div>
+                </div>
+            </div>
+        </body>
+        </html>
+      `,
     });
 
     return {
