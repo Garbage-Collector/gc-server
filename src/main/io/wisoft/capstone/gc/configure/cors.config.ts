0378b6bb3c0f5a3ebc2166024d0c4a82a679d50b
Yu-Jaeyoung <109587069+Yu-Jaeyoung@users.noreply.github.com>
2024-06-22 15:12:39 +0900

    feat: cors-origin add (#25)
    - .env 설정 필요
    
    resolves: #14

--- src/main/io/wisoft/capstone/gc/configure/cors.config.ts
+++ src/main/io/wisoft/capstone/gc/configure/cors.config.ts
@@ -1,9 +1,14 @@
+import * as process from "node:process";
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 import { NestExpressApplication } from "@nestjs/platform-express";
 
 export function corsConfig(app: NestExpressApplication): void {
   const corsOptions: CorsOptions = {
-    origin: ["http://localhost:9000", "http://localhost:9200"],
+    origin: [
+      `http://localhost:${process.env.GC_WEB_SPA}`,
+      `http://localhost:${process.env.GC_WEB_PWA}`,
+      `${process.env.GC_WEB_BUCKET}`,
+    ],
     methods: "GET, POST, PUT, PATCH, DELETE",
   };
 
