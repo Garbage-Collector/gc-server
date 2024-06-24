3e878214304fc2af483e4857206c717ca9a1ec7d
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-06-24 15:51:43 +0900

    feat: jwt 기능 구현
    jwt 기능 구현
    - access 토큰 발급 api 구현
    
    fixes: #15
    related to: #15

--- src/main/io/wisoft/capstone/gc/auth/jwt/jwt.controller.ts
+++ src/main/io/wisoft/capstone/gc/auth/jwt/jwt.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from "@nestjs/common";
+import { RefreshTokenGuard } from "@gc/auth/guard/bearer-token-guard.service";
+import { JwtService } from "@gc/auth/jwt/jwt.service";
+import { Controller, Headers, Post, UseGuards } from "@nestjs/common";
+import { ApiOperation } from "@nestjs/swagger";
 
 @Controller("/api/auth")
-export class JwtController {}
+export class JwtController {
+  constructor(private readonly authService: JwtService) {}
+
+  @Post("token/access")
+  @ApiOperation({
+    summary: "access 토큰 발급",
+    description: "refresh 토큰으로 만료된 access 토큰 발급",
+  })
+  @UseGuards(RefreshTokenGuard)
+  async postAccessToken(
+    @Headers("authorization") rawToken: string,
+  ): Promise<{ accessToken: string }> {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, false);
+
+    return {
+      accessToken: newToken,
+    };
+  }
+}
